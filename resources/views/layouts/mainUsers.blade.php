05683908d9390f94634d46ec7225f91d23bff1e7
Wesley Robert Carneiro Geremias <85595835+Wesley-Robert@users.noreply.github.com>
2022-12-16 01:43:56 -0300

    teste

--- resources/views/layouts/mainUsers.blade.php
+++ resources/views/layouts/mainUsers.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="public/css/mainUsersStyle.css">
-    <link rel="shortcut icon" href="/imgs/acertsoft.png" type="image/png">
+    <link rel="stylesheet" href="/css/mainUsersStyle.css">
+    <link rel="shortcut icon" href="public/imgs/acertsoft.png" type="image/png">
     <link rel="stylesheet" href="/dist/ui/trumbowyg.min.css">
     <title>@yield('Title')</title>
 </head>

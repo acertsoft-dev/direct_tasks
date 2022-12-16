72676545eca1ab73984a29fac919f255d4e72015
Wesley Robert Carneiro Geremias <85595835+Wesley-Robert@users.noreply.github.com>
2022-12-16 01:53:17 -0300

    Update login.blade.php

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -25,7 +25,7 @@
             <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
                 <div class="w-full max-w-md space-y-8">
                     <div>
-                        <img class="mx-auto w-auto logo-login" src="/imgs/acertsoft.png" alt="Your Company">
+                        <img class="mx-auto w-auto logo-login" src="/direct_tasks/public/imgs/acertsoft.png" alt="Your Company">
                         <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Entrar no sistema</h2>
                     </div>
                     <form class="mt-8 space-y-6" action={{ url('login') }} method="POST">

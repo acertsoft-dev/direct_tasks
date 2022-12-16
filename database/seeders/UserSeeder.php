f27990591def6ea256da7128db6b40233f3c0fd3
Wesley Robert Carneiro Geremias <85595835+Wesley-Robert@users.noreply.github.com>
2022-12-16 01:16:41 -0300

    admin add
    Colocado para o usuário Admin ser adicionado ao iniciar o probleto.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'suporte@acertsoft.com.br',
+            'password' => Hash::make('sUp0Rt&2O22') 
+        ]);
+    }
+}

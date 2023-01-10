e33e715dad7a87a24ed7bcb4b6a305d068910d2e
Wesley Robert Carneiro Geremias <85595835+Wesley-Robert@users.noreply.github.com>
2023-01-10 11:22:04 -0300

    Update TasksController.php

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -226,6 +226,6 @@ class TasksController extends Controller
     public function download($file){
 
         // return response()->download(storage_path().'/app/public/'.$file);
-        return response()->download(storage_path().'app/file_tasks/'.$file);
+        return response()->download('direct_tasks/storage/app/file_tasks/'.$file);
     }
 }

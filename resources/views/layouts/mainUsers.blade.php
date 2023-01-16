19e9d927dbc8abe60ffd30c065f1aa7b52a1434a
Wesley Robert Carneiro Geremias <85595835+Wesley-Robert@users.noreply.github.com>
2023-01-16 12:09:26 -0300

    Update mainUsers.blade.php

--- resources/views/layouts/mainUsers.blade.php
+++ resources/views/layouts/mainUsers.blade.php
@@ -40,7 +40,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-    <script src="/js/scripts.js"></script>
+    <script src="direct_tasks/js/scripts.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="/dist/trumbowyg.min.js"></script>
     <script type="text/javascript" src="/dist/langs/fr.min.js"></script>
